45bfd236f455afa73bcc54e4aec7853d1917065e
Mücahit Cücen <mucahitcucen@gmail.com>
2023-10-26 14:01:47 +0300

    remove undefined classes

--- src/Contracts/Masker.php
+++ src/Contracts/Masker.php
@@ -2,9 +2,6 @@
 
 namespace KaracaTech\StringMask\Contracts;
 
-use KaracaTech\StringMask\Contracts\Fluent\Maskable;
-use KaracaTech\StringMask\Contracts\Fluent\WordMasker;
-
 interface Masker extends \Stringable
 {
     public function of(string $target): Masker;
